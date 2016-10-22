2fa6875a494b62aace805eec3e9762adb65324f9
15110671453 <15110671453@163.com>
2016-10-22 15:36:51 +0800

    commit 搞定 nodejs 在sublime调试
    commit 搞定 nodejs 在sublime调试

--- javascript方向nodejs/index.js
+++ javascript方向nodejs/index.js
@@ -1,10 +1,20 @@
 
 console.log('ceshi ');
 var server = require('./server');
-var router = require('./router');
+
 
 console.log(__filename);
 console.log(__dirname);
-server.start(router.route);
+//方式一
+// var router = require('./router');
+//注意这里 router 没有传递参数 文件名 后边出错 
+// server.start(router.route());
+
+//方式二
+
+var Router = require('./router.js');
+//注意这里 router 如果不传递参数 文件名 后边会出错 参数要传递啊
+var r = new Router('Ozzy', 62,'inputtext');
+r.route(); // Ozzy is 62 years old
 
 console.log('ceshi ');
--- javascript方向nodejs/router.js
+++ javascript方向nodejs/router.js
@@ -1,11 +1,26 @@
 
+  console.log(__dirname);
 
-function route (pathname)
-{
+  //方式一
+// function route (pathname)
+// {
 
-	console.log('about to route a request for' + pathname);
-	console.log(__filename);
-    console.log(__dirname);
-}
+// 	console.log('about to route a request for' + pathname);
+// 	console.log(__filename);
+//     console.log(__dirname);
+// }
 
-exports.route = route;
+// exports.route = route;
+
+//方式二
+module.exports = function(name, age,pathname) {
+    this.name = name;
+    this.age = age;
+    this.route = function() {
+        console.log(this.name +' is '+ this.age +' years old');
+         	console.log('about to route a request for' + pathname);
+ 	console.log(__filename);
+     console.log(__dirname);
+    };
+};
+console.log(__dirname);
